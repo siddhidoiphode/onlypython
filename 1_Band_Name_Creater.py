0ab1a2b59304488d931d4ef96383abb7cf7df865
siddhidoiphode <141642695+siddhidoiphode@users.noreply.github.com>
2023-10-11 11:44:57 +0530

    Create 1_Band_Name_Creater.py
    band name creater

---
+++ 1_Band_Name_Creater.py
@@ -0,0 +1,5 @@
+# 1_Band_Name_Creater
+print("Welcome to band name generator.\n")
+n1=input("What's the name of city you grew up in?: ")
+n2=input("What is the name of your pet? :  ")
+print("\nYour Band Name could be : ",n1,n2)
