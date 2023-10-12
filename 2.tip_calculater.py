f237d9c8672cde75b8330632bc3db06d9f2645fe
siddhidoiphode <141642695+siddhidoiphode@users.noreply.github.com>
2023-10-12 08:54:03 +0530

    Update 2.tip_calculater.py
    tip calculator

--- 2.tip_calculater.py
+++ 2.tip_calculater.py
@@ -3,5 +3,6 @@ total=float(input("Enter total amount of bill: "))
 no=int(input("Enter number of members: "))
 tip=int(input("Enter tip percentage: "))
 ans=(total+(total*tip/100))/no
-ans=round(ans,2)
-print("\nYour tip per person is: ",ans)
+#ans=round(ans,2)
+final="{:.2f}".format(ans)
+print("\nYour tip per person is: ",final)
